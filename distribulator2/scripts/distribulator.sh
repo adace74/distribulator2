4404c9e02a688ed7f891e5be0c23e215754b4553
awd <awd@9669c016-0312-0410-afc9-92ac2c5c32b3>
2003-10-02 11:43:44 +0000

    More small updates.
    git-svn-id: https://svn.code.sf.net/p/distribulator/code/trunk@272 9669c016-0312-0410-afc9-92ac2c5c32b3

--- distribulator2/scripts/distribulator.sh
+++ distribulator2/scripts/distribulator.sh
@@ -7,6 +7,6 @@
 # (c) Copyright 2003 Adam W. Dace <adam@turing.com>  All Rights Reserved. 
 # Please see the accompanying LICENSE file for license information.
 #
-INSTALL_DIR="/promo/distribulator2"
+INSTALL_DIR="/usr/local/distribulator2"
 
 $INSTALL_DIR/distribulator.py --directory=$INSTALL_DIR $1 $2 $3 $4 $5 $6 $7 $8
