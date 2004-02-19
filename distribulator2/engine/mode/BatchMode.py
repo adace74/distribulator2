9c04f043b5495fb8a857a7692282ce44f7666d52
awd <awd@9669c016-0312-0410-afc9-92ac2c5c32b3>
2004-02-19 14:47:11 +0000

    Updating a broken log command.
    git-svn-id: https://svn.code.sf.net/p/distribulator/code/trunk@353 9669c016-0312-0410-afc9-92ac2c5c32b3

--- distribulator2/engine/mode/BatchMode.py
+++ distribulator2/engine/mode/BatchMode.py
@@ -234,7 +234,7 @@ class BatchMode(Mode.Mode):
 
                 except KeyboardInterrupt:
                     myInfo = "INFO:  Caught CTRL-C keystroke.  Returning to command prompt..."
-                    self.handleInfo(myInfo)
+                    self._globalConfig.getMultiLogger().LogMsgInfo(myInfo)
 
             myFile.close()
 
