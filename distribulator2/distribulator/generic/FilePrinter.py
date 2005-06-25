fc46fa888da19295ccc817254ec3e4a284a52240
awd <awd@9669c016-0312-0410-afc9-92ac2c5c32b3>
2005-06-25 04:35:52 +0000

    More missed files.  Blah, I miss Subversion.
    git-svn-id: https://svn.code.sf.net/p/distribulator/code/trunk@503 9669c016-0312-0410-afc9-92ac2c5c32b3

---
+++ distribulator2/distribulator/generic/FilePrinter.py
@@ -0,0 +1,45 @@
+######################################################################
+#
+# $Id$
+#
+# (c) Copyright 2004 Orbitz, Inc.  All Rights Reserved. 
+# Please see the accompanying LICENSE file for license information.
+#
+######################################################################
+
+# Pydoc comments
+"""
+Simple generic class whose funciton is to take a given file and
+print it out to STDOUT.
+"""
+
+# Version tag
+__version__= '$Revision$'[11:-2]
+
+######################################################################
+
+class FilePrinter:
+    """
+    Simple generic class whose funciton is to take a given file and
+    print it out to STDOUT.
+    """
+
+    def printFile(self, PassedFilename):
+        """Prints a given file."""
+
+        try:
+            myFile = open(PassedFilename, 'r')
+            
+            for myLine in myFile:
+                myLine = myLine.rstrip()
+                print(myLine)
+
+            myFile.close()
+
+        except IOError, (errno, strerror):
+            print("ERROR: [Errno %s] %s: %s" % (errno, strerror, PassedFilename))
+            return False
+
+        return True
+
+######################################################################
---
+++ distribulator2/distribulator/generic/HostPinger.py
@@ -0,0 +1,38 @@
+######################################################################
+#
+# $Id$
+#
+# (c) Copyright 2004 Orbitz, Inc.  All Rights Reserved. 
+# Please see the accompanying LICENSE file for license information.
+#
+######################################################################
+
+# Pydoc comments
+"""
+Simple generic class whose funciton is to attempt to ping a given host.
+"""
+
+# Version tag
+__version__= '$Revision$'[11:-2]
+
+# Standard modules
+import commands
+
+######################################################################
+
+class HostPinger:
+    """Simple generic class whose funciton is to attempt to ping a given host."""
+
+    def __init__(self, PassedCommand):
+        """Constructor."""
+        self._command = PassedCommand
+
+    def ping(self, PassedHostname):
+        """Attempts to ping a given host."""
+
+        myStatus, myOutput = \
+        commands.getstatusoutput(self._command + " " + PassedHostname)
+
+        return myStatus
+
+#######################################################################
