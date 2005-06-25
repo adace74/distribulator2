70c0812ed08b40f537d99f2f2e9c53031e9047ab
awd <awd@9669c016-0312-0410-afc9-92ac2c5c32b3>
2005-06-25 04:45:22 +0000

    Getting closer to a workable structure...
    git-svn-id: https://svn.code.sf.net/p/distribulator/code/trunk@505 9669c016-0312-0410-afc9-92ac2c5c32b3

--- distribulator2/setup.py
+++ distribulator2/setup.py
@@ -11,33 +11,35 @@
 __version__ = '$Revision$'[11:-2]
 
 # Standard modules
-import os
 import glob
+import os
+import sys
 from distutils.core import setup
 
 # Glob magic courtesy of Brian Zimmer.
-docs =  [x for x in glob.glob('doc/**') if os.path.isfile(x)]
-confs = [x for x in glob.glob('conf/**') if os.path.isfile(x)]
+doc =  [x for x in glob.glob('doc/**') if os.path.isfile(x)]
+conf = [x for x in glob.glob('conf/**') if os.path.isfile(x)]
 examples = [x for x in glob.glob('examples/**') if os.path.isfile(x)]
+src = [x for x in glob.glob('src/**') if os.path.isfile(x)]
 
 # Setup magic courtesy of a combo of Brian Zimmer, Karl D'adamo, and Adace Dace.
 setup(name = "Distribulator",
       author = 'Adam W. Dace',
       author_email = 'adam@turing.com',
       data_files = [
-        ('conf', confs),
-        ('doc', docs),
+        ('conf', conf),
+        ('doc', doc),
         ('log', []),
-        ('examples', examples),
+        ('examples', examples)
       ],
       description = 'Distributed Computing For The Rest Of Us',
       long_description = 'This is an SSH-based command execution and file transfer utility that includes support for batch, console, and shell integration modes, multiple server enviornments, and full audit logs.',
       packages = [
-        'engine', 'engine.command', 'engine.conf',
-        'engine.data', 'engine.misc', 'engine.mode',
-        'generic'
+        'distribulator', 'distribulator.engine', 'distribulator.engine.command',
+        'distribulator.engine.conf', 'distribulator.engine.data', 'distribulator.engine.misc',
+        'distribulator.engine.mode', 'distribulator.generic'
       ],
-      scripts = [ 'distribulator.py', 'scripts/distribulator.sh' ],
+      scripts = [ 'distribulator/scripts/distribulator.sh' ],
       url = 'http://sourceforge.net/projects/distribulator/',
       version = '0.9.1'
 )
